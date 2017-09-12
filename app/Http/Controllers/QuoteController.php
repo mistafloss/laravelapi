cd6e9958249214cace5bdb61d9041ca8c33fe763
Uzoma <u.umekwe@gmail.com>
2017-09-12 18:56:57 +0100

    protecting route with jwt auth

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -10,13 +10,11 @@ use Illuminate\Http\Request;
 class QuoteController extends Controller
 {
     public function postQuote(Request $request){
-        if(! $user = JWTAuth::parseToken()->authenticate()){
-            return response()->json(['message' => 'User not found'], 404);
-        }
+        $user = JWTAuth::parseToken()->toUser();
         $quote = new Quote();
         $quote->content = $request->input('content');
         $quote->save();
-        return response()->json(['quote' => $quote], 201);
+        return response()->json(['quote' => $quote, 'user' => $user], 201);
     }
 
     public function getQuotes(){
